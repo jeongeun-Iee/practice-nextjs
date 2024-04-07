72b046dbf184f7a218424ebc4f508c51323f39ef
Jeongeun Lee <h00kk@naver.com>
2024-04-07 23:33:00 +0900

    create a new project💎

---
+++ app/meals/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main className="not-found">
+      <h1>Meal Not found</h1>
+      <p>요청하신 페이지 혹은 데이터가 없으네여...🙅 지송합니다~💭</p>
+    </main>
+  );
+}
---
+++ app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main className="not-found">
+      <h1>Not found</h1>
+      <p>요청하신 페이지가 없으네여...💭 지송합니다~</p>
+    </main>
+  );
+}
